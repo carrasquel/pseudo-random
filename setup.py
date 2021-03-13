de0ffd6c6eed7f92e822216b507b7e1cd5b4c001
Nelson Carrasquel <carrasquel@outlook.com>
2021-03-12 21:20:01 -0300

    Setup file integrated

---
+++ setup.py
@@ -0,0 +1,18 @@
+# setup.py
+
+from setuptools import setup
+
+setup(name='PseudoRandom',
+    version='0.1',
+    description='A Python package for pseudo random numbers generation',
+    url='https://github.com/carrasquel/pseudo-random',
+    author='Nelson Carrasquel',
+    author_email='carrasquel@outlook.com',
+    license='MIT',
+    packages=['pseudorandom'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    zip_safe=False)
